d482d0e0401bf29f9a570ca01bb5beaf21beb7a4
Trevor Cline <trevorcline@Trevors-MacBook-Pro.local>
2021-04-28 16:29:19 -0500

    Add chapter 11 practice project answers

--- 11-debugging/factorialLog.py
+++ 11-debugging/factorialLog.py
@@ -3,15 +3,18 @@
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
+# logging.disable(logging.CRITICAL)
+## The above line is commented to allow debugging print statements. 
+## Uncommenting that line will remove debug excess output for the user
 logging.debug('Start of program')
 
 def factorial(n):
-    logging.debug('Start of factorial(%s%%)'  % (n))
+    logging.debug('Start of factorial(%s)'  % (n))
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1): # This line was changed to make this program work 
         total *= i
         logging.debug('i is ' + str(i) + ', total is ' + str(total))
-    logging.debug('End of factorial(%s%%)'  % (n))
+    logging.debug('End of factorial(%s)'  % (n))
     return total
 
 print(factorial(5))
